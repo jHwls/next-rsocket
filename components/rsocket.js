31c49b338d79d368e65131990d148c3ced21b165
jhwls <jackhowells@gmail.com>
2021-03-01 19:00:44 -0800

    Trials sending a message

--- components/rsocket.js
+++ components/rsocket.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 
 import RSocketWebSocketClient from "rsocket-websocket-client";
+
 import {
   toBuffer,
   BufferEncoders,
+  FRAME_TYPES,
   MESSAGE_RSOCKET_COMPOSITE_METADATA,
   RSocketClient
 } from "rsocket-core";
@@ -29,6 +31,7 @@ console.log(buffer);
 function Test() {
   const [client, setClient] = React.useState(null);
   const [socket, setSocket] = React.useState(null);
+  const [subId, setSubId] = React.useState(null);
 
   React.useEffect(() => {
     if (client == null) {
@@ -78,9 +81,15 @@ function Test() {
                 console.log(error);
               },
               onNext: (payload) => {
-                console.log(payload);
-                const json = payload.data.toString();
-                console.log(json);
+                console.log("ON NEXT", payload);
+                const data = payload.data.toString();
+                console.log(data);
+                const message = payload.data.toJSON();
+                switch (message.messageType) {
+                  case "I":
+                    setSubId(message.data.subscriptionId);
+                  default:
+                }
               },
               onSubscribe: (subscription) => {
                 subscription.request(2147483647);
@@ -95,6 +104,25 @@ function Test() {
       console.log("connecting!");
     }
   }, [client, socket]);
+
+  React.useEffect(() => {
+    console.log(client, subId, client?._config?.transport?._status?.kind);
+    return;
+    if (!subID || client?._config?.transport?._status?.kind !== "CONNECTED")
+      return;
+
+    console.log("SEND ONE");
+    client._config.transport.sendOne({
+      type: FRAME_TYPES.PAYLOAD,
+      data: toBuffer(
+        JSON.stringify({
+          action: "subscribe",
+          subscriptionId: subId,
+          symbols: ["AAPL"]
+        })
+      )
+    });
+  }, [client, subId]);
 
   if (socket != null && client != null) {
     return <div>Connected - see log!</div>;
